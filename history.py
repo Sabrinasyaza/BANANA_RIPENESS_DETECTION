b74ef3894cb927b19a56f56bc9bba515f35cf03c
Sabrinasyaza <nursabrinasyaza@gmail.com>
2024-01-09 22:30:24 +0800

    baru

--- history.py
+++ history.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 connection = mysql.connector.connect(
     host="localhost",
+    port=3306,
     user="root",
     password="",
     database="banana_ripeness"
--- report.py
+++ report.py
@@ -6,6 +6,7 @@ import mysql.connector
 # Connect to MySQL database
 connection = mysql.connector.connect(
     host="localhost",
+    port=3306,
     user="root",
     password="",
     database="banana_ripeness"
